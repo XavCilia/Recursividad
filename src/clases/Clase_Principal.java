27795b85d47aca61f182eede49492a563e1f20e0
Xavier <99370308+XavCilia@users.noreply.github.com>
2022-03-09 21:54:32 -0600

    Add files via upload

---
+++ src/clases/Clase_Principal.java
@@ -0,0 +1,9 @@
+package clases;
+
+public class Clase_Principal {
+    public static void main(String[] args) {
+        Recursividad recursividad = new Recursividad();
+        recursividad.Imprimir(1);
+    }
+    
+}
